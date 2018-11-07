57c7d733a1d60feed34688dccd94c8beabf304a4
YuWen <31069535+twilighthook@users.noreply.github.com>
2018-11-07 21:24:32 +0800

    The script about data process

---
+++ ReadData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class ReadData : MonoBehaviour {
+
+    player1 p1 = new player1();
+
+    // Use this for initialization
+    void Start () {
+
+        StreamReader fileReader = new StreamReader(System.IO.Path.Combine("Assets/Resources/GameJSONData", "Player1.json"));
+        string loadJson = fileReader.ReadToEnd();
+        fileReader.Close();
+
+        p1 = JsonUtility.FromJson<player1>(loadJson);
+
+        print("Name:" + p1.name);
+
+    }
+
+}
---
+++ SavaData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataSave : MonoBehaviour {
+
+    player1 p1 = new player1();
+
+    void Start(){
+        saveData("Tom" , 20 , 250);
+        string saveString = JsonUtility.ToJson(p1);
+        //將字串saveString存到硬碟中
+        StreamWriter file = new StreamWriter(System.IO.Path.Combine("Assets/Resources/GameJSONData", "Player1.json"));
+        file.Write(saveString);
+        file.Close();
+    }
+
+    private void saveData(string name , int level , int attack){
+        p1.name = name;
+        p1.level = level;
+        p1.attack = attack;
+    }
+
+}
